8abcf50042e0b4de788dd9585d374dfb88250164
Yonatan1203 <yonatanglav67@gmail.com>
2026-04-20 16:34:38 +0300

    refactor: remove free tier limits and weekly product from constants

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -13,16 +13,11 @@ class AppConstants {
   static const String entitlementId = 'GymRatz';
 
   // Product identifiers (must match RevenueCat dashboard)
-  static const String productWeekly = 'weekly';
   static const String productMonthly = 'monthly';
   static const String productYearly = 'yearly';
 
-  // Free tier limits
-  static const int freeMaxActivePrograms = 1;
-  static const int freeHistoryWeeks = 2;
-
   // App info
   static const String appVersion = '1.0.0';
-  static const String privacyPolicyUrl = 'https://gymratz.app/privacy';
-  static const String termsOfServiceUrl = 'https://gymratz.app/terms';
+  static const String privacyPolicyUrl = 'https://gymratz-app.github.io/privacy';
+  static const String termsOfServiceUrl = 'https://gymratz-app.github.io/terms';
 }
